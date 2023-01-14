8cb08cb6bcd51ce037c9e0900943fbeaca593de7
Will <wpm517@gmail.com>
2023-01-13 23:44:21 -0500

    restarting

--- Player.java
+++
@@ -1,7 +0,0 @@
-import java.awt.*;
-import java.awt.geom.*;
-import javax.swing.*;
-
-public class Player extends JComponent {
-    
-}
--- Zombie.java
+++
@@ -1,7 +0,0 @@
-import java.awt.*;
-import java.awt.geom.*;
-import javax.swing.*;
-
-public class Zombie extends JComponent {
-    
-}

9c07d9c03e4861a49343bd4b8b6cc8afc2a72476
Will <wpm517@gmail.com>
2023-01-17 22:53:43 -0500

    finished entity movement and animation

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -8,4 +8,8 @@ public class Entity {
 
     public BufferedImage standingNorth, walkingNorth1, walkingNorth2, standingSouth, walkingSouth1, walkingSouth2, standingEast, walkingEast1, walkingEast2, standingWest, walkingWest1, walkingWest2;
     public String direction;
+    public boolean moving;
+
+    public int animationCounter = 0;
+    public boolean animationStep = false;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -25,56 +25,78 @@ public class Player extends Entity {
         y = 100;
         speed = 4;
         direction = "south";
+        moving = false;
     }
 
     public void getPlayerImage() {
         try {
             standingNorth = ImageIO.read(getClass().getResourceAsStream("/res/player/standing-n.png"));
+            walkingNorth1 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-n1.png"));
+            walkingNorth2 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-n2.png"));
             standingSouth = ImageIO.read(getClass().getResourceAsStream("/res/player/standing-s.png"));
+            walkingSouth1 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-s1.png"));
+            walkingSouth2 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-s2.png"));
             standingEast = ImageIO.read(getClass().getResourceAsStream("/res/player/standing-e.png"));
+            walkingEast1 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-e1.png"));
+            walkingEast2 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-e2.png"));
             standingWest = ImageIO.read(getClass().getResourceAsStream("/res/player/standing-w.png"));
+            walkingWest1 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-w1.png"));
+            walkingWest2 = ImageIO.read(getClass().getResourceAsStream("/res/player/walk-w2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void move() {
-        if (kh.upPressed == true) {
-            direction = "north";
-            y -= speed;
-        }
-        if (kh.downPressed == true) {
-            direction = "south";
-            y += speed;
-        }
-        if (kh.leftPressed == true) {
-            direction = "west";
-            x -= speed;
-        }
-        if (kh.rightPressed == true) {
-            direction = "east";
-            x += speed;
+
+        if (kh.upPressed || kh.downPressed || kh.leftPressed || kh.rightPressed) {
+
+            moving = true;
+
+            if (kh.upPressed) {
+                direction = "north";
+                y -= speed;
+            }
+            if (kh.downPressed) {
+                direction = "south";
+                y += speed;
+            }
+            if (kh.leftPressed) {
+                direction = "west";
+                x -= speed;
+            }
+            if (kh.rightPressed) {
+                direction = "east";
+                x += speed;
+            }
+
+            animationCounter++;
+            if (animationCounter > 15) {
+                animationStep = !animationStep;
+                animationCounter = 0;
+            }
+
+        } else {
+            moving = false;
         }
     }
 
     public void draw(Graphics2D g2d) {
-        // g2d.setColor(Color.WHITE);
-        // g2d.fillRect(x, y, gp.tileSize, gp.tileSize);
 
         BufferedImage image = null;
 
         switch (direction) {
             case "north":
-                image = standingNorth;
+                image = !moving ? standingNorth : animationStep ? walkingNorth1 : walkingNorth2;
                 break;
             case "south":
-                image = standingSouth;
+                image = !moving ? standingSouth : animationStep ? walkingSouth1 : walkingSouth2;
                 break;
             case "east":
-                image = standingEast;
+                image = !moving ? standingEast : animationStep ? walkingEast1 : walkingEast2;
                 break;
             case "west":
-                image = standingWest;
+                image = !moving ? standingWest : animationStep ? walkingWest1 : walkingWest2;
                 break;
         }
 

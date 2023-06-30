40c3ed6909ca82a90199479e7d7ce6e14287e386
Will <wpm517@gmail.com>
2023-06-30 14:57:49 -0400

    removed todos that im not gonna do

--- src/common/Collidable.java
+++ src/common/Collidable.java
@@ -28,7 +28,4 @@ public interface Collidable {
         return this.hasCollision() && other.hasCollision() && this.getHitbox().intersects(other.getHitbox());
     }
 
-    // TODO: make an interface called Movable for things that are movable ... currently only Entity
-    // TODO: instead of the above, start inheriting the different templates in this package so like Collidable inherits Movable or something and Drawable too
-
 }

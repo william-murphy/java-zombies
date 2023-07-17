e6b80a2f0371e57d30ffb41e50eb822abf60d044
Will <wpm517@gmail.com>
2023-07-16 21:03:58 -0400

    moved move methods and things back into Entity superclass

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -10,11 +10,75 @@ public abstract class Entity implements Collidable {
     public Hitbox hitbox;
     public boolean collision = true;
 
+    public int animationCounter = 0;
+    public boolean animationStep = false;
+
+    public int speed;
+    public Direction direction;
+    public boolean moving = false;
+
     // IDEA: For entity collision, what if entity list was priority queue by coordinate somehow, then when checking collision you only have to check the two closest in the x prio q and the y prio q
     // IDEA: make a folder in entity and tile called controller and create a class Controller.java in each with any other classes they may need in there
 
     public Tile getTile() {
         return Tile.getTile((int)hitbox.getCenterX(), (int)hitbox.getCenterY());
+    }
+
+    public void move(Direction direction) {
+        this.moving = true;
+        this.direction = direction;
+    }
+
+    public void stopMove(Direction direction) {
+        if (this.direction == direction) {
+            this.moving = false;
+        }
+    }
+
+    protected void updatePosition() {
+        if (moving) {
+            switch(direction) {
+                case NORTH:
+                    hitbox.translate(0, -speed);
+                    if (collides(Tile.getTile(hitbox.x, hitbox.y))) {
+                        hitbox.setLocation(hitbox.x, (int)Tile.getTile(hitbox.x, hitbox.y).getHitbox().getMaxY());
+                    } else if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y))) {
+                        hitbox.setLocation(hitbox.x, (int)Tile.getTile(hitbox.x + hitbox.width, hitbox.y).getHitbox().getMaxY());
+                    }
+                    break;
+                case SOUTH:
+                    hitbox.translate(0, speed);
+                    if (collides(Tile.getTile(hitbox.x, hitbox.y + hitbox.height))) {
+                        hitbox.setLocation(hitbox.x, Tile.getTile(hitbox.x, hitbox.y + hitbox.height).getHitbox().y - hitbox.height);
+                    } else if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height))) {
+                        hitbox.setLocation(hitbox.x, Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height).getHitbox().y - hitbox.height);
+                    }
+                    break;
+                case EAST:
+                    hitbox.translate(speed, 0);
+                    if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y))) {
+                        hitbox.setLocation(Tile.getTile(hitbox.x + hitbox.width, hitbox.y).getHitbox().x - hitbox.width, hitbox.y);
+                    } else if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height))) {
+                        hitbox.setLocation(Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height).getHitbox().x - hitbox.width, hitbox.y);
+                    }
+                    break;
+                case WEST:
+                    hitbox.translate(-speed, 0);
+                    if (collides(Tile.getTile(hitbox.x, hitbox.y))) {
+                        hitbox.setLocation((int)Tile.getTile(hitbox.x, hitbox.y).getHitbox().getMaxX(), hitbox.y);
+                    } else if (collides(Tile.getTile(hitbox.x, hitbox.y + hitbox.height))) {
+                        hitbox.setLocation((int)Tile.getTile(hitbox.x, hitbox.y + hitbox.height).getHitbox().getMaxX(), hitbox.y);
+                    }
+                    break;
+            }
+
+            animationCounter++;
+            if (animationCounter > 15) {
+                animationStep = !animationStep;
+                animationCounter = 0;
+            }
+
+        }
     }
 
     public boolean fitsHorizontally(Tile tile) {
--- src/entity/creature/Creature.java
+++ src/entity/creature/Creature.java
@@ -1,79 +1,13 @@
 package entity.creature;
 
 import entity.Entity;
-import tile.Tile;
-import common.Direction;
 
 import java.awt.Point;
 
 public abstract class Creature extends Entity {
-    
-    public int animationCounter = 0;
-    public boolean animationStep = false;
-
-    public int speed;
-    public Direction direction;
-    public boolean moving = false;
 
     public int maxHealth;
     public int health;
-
-    public void move(Direction direction) {
-        this.moving = true;
-        this.direction = direction;
-    }
-
-    public void stopMove(Direction direction) {
-        if (this.direction == direction) {
-            this.moving = false;
-        }
-    }
-
-    protected void updatePosition() {
-        if (moving) {
-            switch(direction) {
-                case NORTH:
-                    hitbox.translate(0, -speed);
-                    if (collides(Tile.getTile(hitbox.x, hitbox.y))) {
-                        hitbox.setLocation(hitbox.x, (int)Tile.getTile(hitbox.x, hitbox.y).getHitbox().getMaxY());
-                    } else if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y))) {
-                        hitbox.setLocation(hitbox.x, (int)Tile.getTile(hitbox.x + hitbox.width, hitbox.y).getHitbox().getMaxY());
-                    }
-                    break;
-                case SOUTH:
-                    hitbox.translate(0, speed);
-                    if (collides(Tile.getTile(hitbox.x, hitbox.y + hitbox.height))) {
-                        hitbox.setLocation(hitbox.x, Tile.getTile(hitbox.x, hitbox.y + hitbox.height).getHitbox().y - hitbox.height);
-                    } else if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height))) {
-                        hitbox.setLocation(hitbox.x, Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height).getHitbox().y - hitbox.height);
-                    }
-                    break;
-                case EAST:
-                    hitbox.translate(speed, 0);
-                    if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y))) {
-                        hitbox.setLocation(Tile.getTile(hitbox.x + hitbox.width, hitbox.y).getHitbox().x - hitbox.width, hitbox.y);
-                    } else if (collides(Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height))) {
-                        hitbox.setLocation(Tile.getTile(hitbox.x + hitbox.width, hitbox.y + hitbox.height).getHitbox().x - hitbox.width, hitbox.y);
-                    }
-                    break;
-                case WEST:
-                    hitbox.translate(-speed, 0);
-                    if (collides(Tile.getTile(hitbox.x, hitbox.y))) {
-                        hitbox.setLocation((int)Tile.getTile(hitbox.x, hitbox.y).getHitbox().getMaxX(), hitbox.y);
-                    } else if (collides(Tile.getTile(hitbox.x, hitbox.y + hitbox.height))) {
-                        hitbox.setLocation((int)Tile.getTile(hitbox.x, hitbox.y + hitbox.height).getHitbox().getMaxX(), hitbox.y);
-                    }
-                    break;
-            }
-
-            animationCounter++;
-            if (animationCounter > 15) {
-                animationStep = !animationStep;
-                animationCounter = 0;
-            }
-
-        }
-    }
 
     public abstract Point getHand();
 

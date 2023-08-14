bb3791d3cbe197ae65bdb3231b23772e8e205959
Will <wpm517@gmail.com>
2023-08-13 20:19:38 -0400

    added controls to main menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -4,6 +4,7 @@ import main.Window;
 import game.Game;
 
 import javax.swing.JPanel;
+import javax.swing.Box;
 import javax.swing.JLabel;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -14,7 +15,15 @@ public class Menu extends JPanel {
 
     KeyHandler keyHandler = new KeyHandler();
 
-    JLabel instruction = new JLabel("Press Any Key to Start");
+    final Box textPanel = Box.createVerticalBox();
+    final JLabel instruction = new JLabel("Press Any Key to Start");
+    final JLabel controlText = new JLabel("Controls:");
+    final JLabel control1 = new JLabel("- Move with WASD");
+    final JLabel control2 = new JLabel("- Walk over items to pick them up");
+    final JLabel control3 = new JLabel("- Drop item with P");
+    final JLabel control4 = new JLabel("- Reload with L");
+    final JLabel control5 = new JLabel("- Shoot with K");
+    final JLabel control6 = new JLabel("- Show debug mode with tilda(~)");
 
     public Menu() {
         this.setPreferredSize(new Dimension(Game.screenWidth, Game.screenHeight));
@@ -22,7 +31,16 @@ public class Menu extends JPanel {
         this.setFocusable(true);
 
         //add the components
-        this.add(instruction);
+        textPanel.add(instruction);
+        textPanel.add(instruction);
+        textPanel.add(controlText);
+        textPanel.add(control1);
+        textPanel.add(control2);
+        textPanel.add(control3);
+        textPanel.add(control4);
+        textPanel.add(control5);
+        textPanel.add(control6);
+        this.add(textPanel);
 
         //add the key listener
         this.addKeyListener(keyHandler);
